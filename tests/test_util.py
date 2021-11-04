0447978c67f8ae0525dab46dc4c153b88093da33
KLB <k.besser@tu-bs.de>
2021-11-04 15:19:02 +0100

    Add more tests of dupliation matrices

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,8 +6,12 @@ from secrecy_capacity import duplication_matrix_fast
 
 
 _expected_dup_2 = np.array([[1, 0, 0], [0, 1, 0], [0, 1, 0], [0, 0, 1]])
+_expected_dup_3 = np.array([[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1]])
+_expected_dup_4 = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
 
-@pytest.mark.parametrize("n,expected", [(2, _expected_dup_2)])
+@pytest.mark.parametrize("n,expected", [(2, _expected_dup_2),
+                                        (3, _expected_dup_3),
+                                        (4, _expected_dup_4)])
 def test_duplication_matrix(n, expected):
     dup = duplication_matrix_fast(n)
     expected_sparse = sparse.csr_matrix(expected)
