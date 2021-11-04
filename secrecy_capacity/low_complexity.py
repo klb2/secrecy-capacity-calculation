0b5d5f9bcb56f9c08e1a989172dabdca09f20d17
KLB <k.besser@tu-bs.de>
2021-11-04 15:05:46 +0100

    Remove use of np.matrix in low complexity implementation

--- secrecy_capacity/low_complexity.py
+++ secrecy_capacity/low_complexity.py
@@ -28,14 +28,14 @@ import logging
 import numpy as np
 from scipy.linalg import sqrtm
 
+from .util import H
+
 LOGGER = logging.getLogger("low_complexity_algorithm")
 
 
 def cov_secrecy_capacity_low_complexity(mat_bob, mat_eve, power=1, tol_eps=1e-6):
     time0 = time()
     # Get all parameters
-    mat_bob = np.matrix(mat_bob)
-    mat_eve = np.matrix(mat_eve)
     n_rx, n_tx = np.shape(mat_bob)
     n_eve = np.shape(mat_eve)[0]
     LOGGER.debug("Matrices have shape: %d x %d", n_rx, n_tx)
@@ -52,8 +52,8 @@ def cov_secrecy_capacity_low_complexity(mat_bob, mat_eve, power=1, tol_eps=1e-6)
     while abs(_obj_out_new - _obj_out_old) > 1e-6:
         t_start = time()
         _obj_out_old = _obj_out_new
-        inv_cap_eve = np.linalg.inv(np.eye(n_eve) + mat_eve@cov@mat_eve.H)
-        mat_q = mat_eve.H @ inv_cap_eve @ mat_eve  # Q_{t}
+        inv_cap_eve = np.linalg.inv(np.eye(n_eve) + mat_eve @ cov @ H(mat_eve))
+        mat_q = H(mat_eve) @ inv_cap_eve @ mat_eve  # Q_{t}
         n = 0
         mat_k = np.copy(mat_omega)  # K_{0}
 
@@ -62,12 +62,12 @@ def cov_secrecy_capacity_low_complexity(mat_bob, mat_eve, power=1, tol_eps=1e-6)
         while abs(_obj_inner_new - _obj_inner_old) > 1e-4:
             _obj_inner_old = _obj_inner_new
             mat_w = _algorithm1(h_bar, mat_k, mat_q, power=power)  # W_{n}
-            mat_t = np.linalg.inv(mat_k + h_bar@mat_w@h_bar.H)  # T_{n}
+            mat_t = np.linalg.inv(mat_k + h_bar @ mat_w @ H(h_bar))  # T_{n}
             mat_t_bar = mat_t[-n_eve:, :n_rx]  # \bar{T}_{n}
-            _rho, mat_u = np.linalg.eigh(mat_t_bar @ mat_t_bar.H)  # (16)
+            _rho, mat_u = np.linalg.eigh(mat_t_bar @ H(mat_t_bar))  # (16)
             _delta = 2*np.diag(1./(1+np.sqrt(1+4*_rho)))  # (16)
-            mat_k_bar = -mat_u @ _delta @ mat_u.H @ mat_t_bar  # \bar{K}_{n+1}
-            mat_k = np.block([[np.eye(n_rx), mat_k_bar.H], [mat_k_bar, np.eye(n_eve)]])
+            mat_k_bar = -mat_u @ _delta @ H(mat_u) @ mat_t_bar  # \bar{K}_{n+1}
+            mat_k = np.block([[np.eye(n_rx), H(mat_k_bar)], [mat_k_bar, np.eye(n_eve)]])
             n = n+1
             _obj_inner_new = objective_inner(mat_t, mat_k)
             #print(_obj_inner_new-_obj_inner_old)
@@ -95,9 +95,9 @@ def _algorithm1(h_bar, mat_k, mat_q, power=1, tol_eps=1e-12):
         #inv_sqrt_m = np.linalg.inv(sqrtm(mat_m))
         _sqrt_m = sqrtm(mat_m)
         inv_sqrt_m = np.linalg.inv(_sqrt_m)
-        eig_val, eig_vec = np.linalg.eigh(inv_sqrt_m @ h_bar.H @ inv_k @ h_bar @ inv_sqrt_m)
+        eig_val, eig_vec = np.linalg.eigh(inv_sqrt_m @ H(h_bar) @ inv_k @ h_bar @ inv_sqrt_m)
         mat_phi = np.diag(np.maximum(1-1./eig_val, 0))
-        mat_w = inv_sqrt_m @ eig_vec @ mat_phi @ eig_vec.H @ inv_sqrt_m
+        mat_w = inv_sqrt_m @ eig_vec @ mat_phi @ H(eig_vec) @ inv_sqrt_m
         if np.trace(mat_w) > power:
             mu_1 = mu
         else:
@@ -106,10 +106,10 @@ def _algorithm1(h_bar, mat_k, mat_q, power=1, tol_eps=1e-12):
 
 def _func_ft(h_bar, mat_omega, cov, mat_eve):
     n_eve = len(mat_eve)
-    _part1 = np.log(np.linalg.det(mat_omega + h_bar @ cov @ h_bar.H))
+    _part1 = np.log(np.linalg.det(mat_omega + h_bar @ cov @ H(h_bar)))
     _part2 = np.log(np.linalg.det(mat_omega))
     #_part3 = np.trace(
-    _part4 = np.log(np.linalg.det(np.eye(n_eve) + mat_eve @ cov @ mat_eve.H))
+    _part4 = np.log(np.linalg.det(np.eye(n_eve) + mat_eve @ cov @ H(mat_eve)))
     return _part1 - _part2 - _part4
 
 def objective_inner(mat_t, mat_k):
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -8,18 +8,18 @@ from secrecy_capacity import low_complexity
 # Example from Loyka's paper (see eq (63))
 BOB = np.array([[.77, -.3], [-.32, -.64]])
 EVE = np.array([[.54, -.11], [-.93, -1.71]])
-EXPECT_SEC_CAP = 0.717  # estimated from Fig. 3 in Loykas paper
+EXPECT_SEC_CAP = 0.717/np.log(2)  # estimated from Fig. 3 in Loykas paper in bits
 
 def test_loyka():
     cov = loyka_algorithm.cov_secrecy_capacity_loyka(BOB, EVE, power=10,
             t=1e5, alpha=.3, beta=.5, eps=1e-8, dirname=None, return_interm_results=False)
     sec_cap = calculations_physec.secrecy_rate(BOB, EVE, cov=cov)
-    print(sec_cap)
-    assert np.isclose(sec_cap, EXPECT_SEC_CAP, atol=1e-3)
+    print(sec_cap, EXPECT_SEC_CAP)
+    assert np.isclose(sec_cap, EXPECT_SEC_CAP, atol=2e-3)
 
 
 def test_low_compl():
     cov = low_complexity.cov_secrecy_capacity_low_complexity(BOB, EVE, power=10)
     sec_cap = calculations_physec.secrecy_rate(BOB, EVE, cov=cov)
-    print(sec_cap)
-    assert np.isclose(sec_cap, EXPECT_SEC_CAP, atol=1e-3)
+    print(sec_cap, EXPECT_SEC_CAP)
+    assert np.isclose(sec_cap, EXPECT_SEC_CAP, atol=2e-3)
